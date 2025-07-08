947957f3638b9b9214d0e3a77fb0d2d16f975242
yamana711 <e1q22085@oit.ac.jp>
2025-07-08 17:30:23 +0900

    playerの追加

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.List;
+
 public class App {
     public String getGreeting() {
         return "こんにちは SEMonster!";
@@ -13,6 +15,15 @@ public class App {
 
         Monster monster = Monster.summonMonster(2);
         System.out.println("召喚モンスター1: " + monster);
+
+        // 1. デッキに入れるモンスターのレア度をリストで決める
+        List<Integer> rarities = List.of(1, 3, 4, 2, 0);
+
+        // 2. プレイヤーを生成する (名前:"Taro", デッキは上記リストの内容)
+        Player player1 = new Player("Taro", rarities);
+
+        // 3. プレイヤーの情報を表示する (PlayerクラスのtoString()が呼ばれる)
+        System.out.println(player1);
 
     }
 }
---
+++ app/src/test/java/org/example/PlayerTest.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+
+class PlayerTest {
+    @Test
+    void testPlayerToString() {
+// 1. (Arrange) 準備: テスト用のプレイヤーを作成
+        // レア度 0, 4, 0, 6, 1 のモンスターを持つ "user" という名前のプレイヤー
+        List<Integer> rarities = List.of(0, 4, 6, 1, 3);
+        Player player = new Player("user", rarities);
+
+        // 2. (Act) 実行: toString() メソッドを呼び出す
+        String actualOutput = player.toString();
+
+        // 3. (Assert) 検証: 期待される出力と実際の結果が一致するか確認
+        String expectedOutput =
+                "Deck:user\n" +
+                "ゴブリン:レア度[0]\n" +
+                "ベヒーモス:レア度[4]\n" +
+                "フェニックス:レア度[6]\n" +
+                "オーク:レア度[1]\n" +
+                "リザードマン:レア度[3]\n";
+
+        assertEquals(expectedOutput, actualOutput);
+    }
+}
